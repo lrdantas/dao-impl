fd30b5ce7db30fb91c479b12a81428d832c4fce6
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-10-14 11:02:19 -0300

    melhorado script de criação

--- create.sql
+++ create.sql
@@ -1,11 +1,16 @@
 
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET default_storage_engine=InnoDB */;
 
 CREATE DATABASE IF NOT EXISTS `dao_testing`
-/*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+/*!40100 CHARACTER SET utf8mb4 */
+/*!40100 COLLATE utf8mb4_general_ci */
+/*!40100 DEFAULT CHARACTER SET utf8mb4 */
+/*!40100 DEFAULT COLLATE utf8mb4_general_ci */;
 
 USE `dao_testing`;
+
 
 CREATE TABLE IF NOT EXISTS `usuario` (
 
@@ -15,6 +20,5 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   PRIMARY KEY (`id`)
 
 )
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
+
 ;
